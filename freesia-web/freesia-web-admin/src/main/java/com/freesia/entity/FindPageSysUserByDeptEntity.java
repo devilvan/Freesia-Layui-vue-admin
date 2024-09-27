fd552a3b492996533691f3143e6da3ba78eb1e92
Devilvan <1005338848@qq.com>
2024-09-27 15:47:10 +0800

    【修复】Evad 1. 管理员部门查询功能异常问题处理

--- freesia-web/freesia-web-admin/src/main/java/com/freesia/entity/FindPageSysUserByDeptEntity.java
+++ freesia-web/freesia-web-admin/src/main/java/com/freesia/entity/FindPageSysUserByDeptEntity.java
@@ -1,7 +1,9 @@
 package com.freesia.entity;
 
 import com.alibaba.fastjson.annotation.JSONField;
+import com.freesia.annotation.Desensitize;
 import com.freesia.constant.Constants;
+import com.freesia.constant.DesensitizedType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -26,8 +28,10 @@ public class FindPageSysUserByDeptEntity extends BaseEntity {
     @Schema(description = "用户名")
     private String userName;
     @Schema(description = "邮箱")
+    @Desensitize(strategy = DesensitizedType.EMAIL)
     private String email;
     @Schema(description = "电话号码")
+    @Desensitize(strategy = {DesensitizedType.MOBILE_PHONE, DesensitizedType.FIXED_PHONE})
     private String telNo;
     @Schema(description = "性别")
     private String gender;
