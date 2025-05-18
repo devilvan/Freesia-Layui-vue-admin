4a0a563a7b312d74b4998be50ba5b6e54e15997f
Devilvan <1005338848@qq.com>
2025-05-18 23:53:29 +0800

    【修复】Evad 1. 接入图标模板缺陷修复

--- freesia-common/freesia-common-oss/src/main/java/com/freesia/oss/seder/DomainSeValueFilter.java
+++ freesia-common/freesia-common-oss/src/main/java/com/freesia/oss/seder/DomainSeValueFilter.java
@@ -5,7 +5,10 @@ import com.freesia.oss.annotation.Domain;
 import com.freesia.oss.pojo.OssFactory;
 import com.freesia.oss.pojo.OssHandler;
 import com.freesia.util.UEmpty;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 
 /**
@@ -16,8 +19,10 @@ import java.lang.reflect.Field;
 public class DomainSeValueFilter implements ValueFilter {
     @Override
     public Object process(Object object, String name, Object value) {
+        Class<?> clz = object.getClass();
         try {
-            Field field = object.getClass().getDeclaredField(name);
+            boolean flag = hasAnnotationInClassHierarchy(clz, Domain.class);
+            Field field = clz.getDeclaredField(name);
             Domain domain = field.getAnnotation(Domain.class);
             if (domain == null) {
                 return value;
@@ -39,5 +44,25 @@ public class DomainSeValueFilter implements ValueFilter {
             return value;
         }
         return value;
+    }
+
+    public static boolean hasAnnotationInClassHierarchy(Class<?> clazz, Class<? extends Annotation> annotationType) {
+        // 检查当前类及其父类
+        while (clazz != null && clazz != Object.class) {
+            // 检查类上的注解
+            if (AnnotationUtils.findAnnotation(clazz, annotationType) != null) {
+                return clazz.getField();
+            }
+
+            // 检查所有字段上的注解
+            ReflectionUtils.doWithFields(clazz, field -> {
+                if (field.getAnnotation(annotationType) != null) {
+                    throw new RuntimeException("Found annotation on field: " + field.getName());
+                }
+            });
+
+            clazz = clazz.getSuperclass();
+        }
+        return false;
     }
 }
