be8afa4c7de3a1ff2ec0a8a65b6ecc3337828492
Devilvan <1005338848@qq.com>
2025-05-19 18:13:22 +0800

    【修复】Evad 1. 接入图标模板缺陷修复

--- freesia-common/freesia-common-framework/src/main/java/com/freesia/util/UReflect.java
+++ freesia-common/freesia-common-framework/src/main/java/com/freesia/util/UReflect.java
@@ -4,6 +4,14 @@ import cn.hutool.core.util.ReflectUtil;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Evad.Wu
@@ -18,6 +26,42 @@ public class UReflect extends ReflectUtil {
     private static final String GETTER_PREFIX = "get";
 
     private static final String SEPARATOR = ".";
+
+    private static final Map<Class<?>, Map<Class<? extends Annotation>, List<Field>>> CACHE = new ConcurrentHashMap<>();
+
+    /**
+     * 查找类及其父类中带有指定注解的属性（包括元注解）
+     *
+     * @param clazz          要检查的类
+     * @param annotationType 要查找的注解类型
+     * @return 包含所有匹配Field的列表
+     */
+    public static List<Field> findFieldsWithAnnotation(Class<?> clazz,
+                                                       Class<? extends Annotation> annotationType) {
+        return CACHE.computeIfAbsent(clazz, k -> new ConcurrentHashMap<>())
+                .computeIfAbsent(annotationType, k -> findAllFieldsWithAnnotation(clazz, annotationType));
+    }
+
+    /**
+     * 查找类及其父类中带有指定注解的属性（包括元注解）
+     *
+     * @param clazz          要检查的类
+     * @param annotationType 要查找的注解类型
+     * @return 包含所有匹配Field的列表
+     */
+    private static List<Field> findAllFieldsWithAnnotation(Class<?> clazz, Class<? extends Annotation> annotationType) {
+        List<Field> result = new ArrayList<>();
+        Class<?> currentClass = clazz;
+        while (currentClass != null && currentClass != Object.class) {
+            for (Field field : currentClass.getDeclaredFields()) {
+                if (AnnotatedElementUtils.hasAnnotation(field, annotationType)) {
+                    result.add(field);
+                }
+            }
+            currentClass = currentClass.getSuperclass();
+        }
+        return result;
+    }
 
     /**
      * 调用Getter方法.
--- freesia-common/freesia-common-oss/src/main/java/com/freesia/oss/seder/DomainSeValueFilter.java
+++ freesia-common/freesia-common-oss/src/main/java/com/freesia/oss/seder/DomainSeValueFilter.java
@@ -5,11 +5,10 @@ import com.freesia.oss.annotation.Domain;
 import com.freesia.oss.pojo.OssFactory;
 import com.freesia.oss.pojo.OssHandler;
 import com.freesia.util.UEmpty;
-import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.util.ReflectionUtils;
+import com.freesia.util.UReflect;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.List;
 
 /**
  * @author Bliss.Wu
@@ -20,49 +19,36 @@ public class DomainSeValueFilter implements ValueFilter {
     @Override
     public Object process(Object object, String name, Object value) {
         Class<?> clz = object.getClass();
-        try {
-            boolean flag = hasAnnotationInClassHierarchy(clz, Domain.class);
-            Field field = clz.getDeclaredField(name);
-            Domain domain = field.getAnnotation(Domain.class);
-            if (domain == null) {
-                return value;
-            }
-            String configKey = domain.configKey();
-            OssHandler ossHandler;
-            if (UEmpty.isNotEmpty(configKey)) {
-                ossHandler = OssFactory.getInstance(configKey);
-            } else {
-                ossHandler = OssFactory.getInstance();
-            }
-            if (value instanceof String) {
-                if (UEmpty.isEmpty(value)) {
-                    return value;
+        List<Field> fieldList = UReflect.findFieldsWithAnnotation(clz, Domain.class);
+        Field field = null;
+        if (UEmpty.isNotEmpty(fieldList)) {
+            for (Field item : fieldList) {
+                if (name.equals(item.getName())) {
+                    field = item;
+                    break;
                 }
-                return ossHandler.convertEndpoint2Domain(value.toString());
             }
-        } catch (NoSuchFieldException e) {
+        }
+        if (UEmpty.isNull(field)) {
             return value;
         }
-        return value;
-    }
-
-    public static boolean hasAnnotationInClassHierarchy(Class<?> clazz, Class<? extends Annotation> annotationType) {
-        // 检查当前类及其父类
-        while (clazz != null && clazz != Object.class) {
-            // 检查类上的注解
-            if (AnnotationUtils.findAnnotation(clazz, annotationType) != null) {
-                return clazz.getField();
-            }
-
-            // 检查所有字段上的注解
-            ReflectionUtils.doWithFields(clazz, field -> {
-                if (field.getAnnotation(annotationType) != null) {
-                    throw new RuntimeException("Found annotation on field: " + field.getName());
-                }
-            });
-
-            clazz = clazz.getSuperclass();
+        Domain domain = field.getAnnotation(Domain.class);
+        if (domain == null) {
+            return value;
         }
-        return false;
+        String configKey = domain.configKey();
+        OssHandler ossHandler;
+        if (UEmpty.isNotEmpty(configKey)) {
+            ossHandler = OssFactory.getInstance(configKey);
+        } else {
+            ossHandler = OssFactory.getInstance();
+        }
+        if (value instanceof String) {
+            if (UEmpty.isEmpty(value)) {
+                return value;
+            }
+            return ossHandler.convertEndpoint2Domain(value.toString());
+        }
+        return value;
     }
 }
