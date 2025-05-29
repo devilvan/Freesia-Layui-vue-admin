4f60694e1ede7e7bbe56bafece0666b1faa54f43
Devilvan <1005338848@qq.com>
2025-05-30 00:11:51 +0800

    【新增】Evad 1. 接入spring-admin

--- freesia-web/freesia-web-app/src/main/java/com/freesia/FreesiaAdminApplication.java
+++ freesia-web/freesia-web-app/src/main/java/com/freesia/FreesiaAdminApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAsync
 @EnableCaching
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @SpringBootApplication(scanBasePackages = "com.freesia")
