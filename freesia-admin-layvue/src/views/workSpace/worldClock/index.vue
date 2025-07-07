a90215b1d26230e758376b80b547c51bec6b4ffc
Devilvan <1005338848@qq.com>
2025-07-07 09:51:06 +0800

    【修改】Evad 1. 修改国旗图标名称

--- freesia-admin-layvue/src/views/workSpace/worldClock/index.vue
+++ freesia-admin-layvue/src/views/workSpace/worldClock/index.vue
@@ -4,7 +4,7 @@
     <div class="clocks-container">
       <div v-for="clock in clocks" :key="clock.timezone" class="clock-card">
         <div class="location">
-          <lay-avatar :src="clock.flag"></lay-avatar>
+          <lay-avatar size="lg" :src="clock.flag"></lay-avatar>
           {{ clock.location }}
         </div>
         <div class="time">{{ clock.time }}:{{ clock.seconds < 10 ? '0' + clock.seconds : clock.seconds }}</div>
@@ -33,18 +33,18 @@ export default defineComponent({
   name: 'WorldClock',
   setup() {
     const clocks = reactive<Clock[]>([
-      {location: '上海', timezone: 'Asia/Shanghai', flag: '../../../src/assets/flag/中国国旗.svg'},
-      {location: '东京', timezone: 'Asia/Tokyo', flag: '../../../src/assets/flag/日本国旗.svg'},
-      {location: '伦敦', timezone: 'Europe/London', flag: '../../../src/assets/flag/英国国旗.svg'},
-      {location: '柏林', timezone: 'Europe/Berlin', flag: '../../../src/assets/flag/德国.svg'},
-      {location: '纽约（美东）', timezone: 'America/New_York', flag: '../../../src/assets/flag/美国国旗.svg'},
-      {location: '丹佛（美中）', timezone: 'America/Denver', flag: '../../../src/assets/flag/美国国旗.svg'},
-      {location: '洛杉矶（美西）', timezone: 'America/Los_Angeles', flag: '../../../src/assets/flag/美国国旗.svg'},
-      {location: '悉尼', timezone: 'Australia/Sydney', flag: '../../../src/assets/flag/澳大利亚国旗.svg'},
-      {location: '布宜诺斯艾利斯', timezone: 'America/Argentina/Buenos_Aires', flag: '../../../src/assets/flag/阿根廷.svg'},
-      {location: '圣地亚哥', timezone: 'America/Santiago', flag: '../../../src/assets/flag/智利.svg'},
-      {location: '利雅得', timezone: 'Asia/Riyadh', flag: '../../../src/assets/flag/沙特阿拉伯.svg'},
-      {location: '约翰内斯堡', timezone: 'Africa/Johannesburg', flag: '../../../src/assets/flag/South Africa.svg'},
+      {location: '上海', timezone: 'Asia/Shanghai', flag: '/flag/China.svg'},
+      {location: '东京', timezone: 'Asia/Tokyo', flag: '/flag/Japan.svg'},
+      {location: '伦敦', timezone: 'Europe/London', flag: '/flag/Britain.svg'},
+      {location: '柏林', timezone: 'Europe/Berlin', flag: '/flag/Germany.svg'},
+      {location: '纽约（美东）', timezone: 'America/New_York', flag: '/flag/United States.svg'},
+      {location: '丹佛（美中）', timezone: 'America/Denver', flag: '/flag/United States.svg'},
+      {location: '洛杉矶（美西）', timezone: 'America/Los_Angeles', flag: '/flag/United States.svg'},
+      {location: '悉尼', timezone: 'Australia/Sydney', flag: '/flag/Australia.svg'},
+      {location: '布宜诺斯艾利斯', timezone: 'America/Argentina/Buenos_Aires', flag: '/flag/Argentina.svg'},
+      {location: '圣地亚哥', timezone: 'America/Santiago', flag: '/flag/Chile.svg'},
+      {location: '利雅得', timezone: 'Asia/Riyadh', flag: '/flag/Saudi.svg'},
+      {location: '约翰内斯堡', timezone: 'Africa/Johannesburg', flag: '/flag/South Africa.svg'},
     ].map(clock => ({
       ...clock,
       time: '',
