458978c489ca9ae62db9b1668e71de9434fb5311
Devilvan <1005338848@qq.com>
2026-08-20 15:42:36 +0800

    【修复】Evad 1. 接入多数据源后Druid无法正常监控SQL缺陷修复 2. 多数据源配置druid配置下沉到配置类中

--- freesia-common/freesia-common-jdbc/src/main/java/com/freesia/jdbc/config/MybatisConfig.java
+++ freesia-common/freesia-common-jdbc/src/main/java/com/freesia/jdbc/config/MybatisConfig.java
@@ -1,49 +1,33 @@
 package com.freesia.jdbc.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
+import com.alibaba.druid.DbType;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot3.autoconfigure.DruidDataSourceAutoConfigure;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
-import com.alibaba.druid.spring.boot3.autoconfigure.DruidDataSourceAutoConfigure;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
-/**
- * @author Evad.Wu
- * @Description Mybatis配置类
- * PaginationInnerInterceptor 分页插件，自动识别数据库类型
- * https://baomidou.com/pages/97710a/
- * OptimisticLockerInnerInterceptor 乐观锁插件
- * https://baomidou.com/pages/0d93c0/
- * MetaObjectHandler 元对象字段填充控制器
- * https://baomidou.com/pages/4c6bcf/
- * ISqlInjector sql注入器
- * https://baomidou.com/pages/42ea4a/
- * BlockAttackInnerInterceptor 如果是对全表的删除或更新操作，就会终止该操作
- * https://baomidou.com/pages/f9a237/
- * IllegalSQLInnerInterceptor sql性能规范插件(垃圾SQL拦截)
- * IdentifierGenerator 自定义主键策略
- * https://baomidou.com/pages/568eb2/
- * TenantLineInnerInterceptor 多租户插件
- * https://baomidou.com/pages/aef2f2/
- * DynamicTableNameInnerInterceptor 动态表名插件
- * https://baomidou.com/pages/2a45ff/
- * @date 2022-07-13
- */
 @AutoConfiguration
 @AutoConfigureBefore({
         DruidDataSourceAutoConfigure.class,
@@ -51,19 +35,55 @@ import javax.sql.DataSource;
         MybatisPlusAutoConfiguration.class
 })
 public class MybatisConfig {
-    /**
-     * 创建 MySQL 数据源
-     */
     @Primary
     @Bean(name = "mysqlDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.mysql")
-    public DataSource mysqlDataSource() {
-        return DataSourceBuilder.create().build();
+    public DruidDataSource mysqlDataSource(Environment environment) {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setName("mysql");
+        dataSource.setDriverClassName(environment.getRequiredProperty("spring.datasource.mysql.driver-class-name"));
+        dataSource.setUrl(environment.getRequiredProperty("spring.datasource.mysql.jdbc-url"));
+        dataSource.setUsername(environment.getRequiredProperty("spring.datasource.mysql.username"));
+        dataSource.setPassword(environment.getRequiredProperty("spring.datasource.mysql.password"));
+        dataSource.setDbType(DbType.mysql);
+        dataSource.setInitialSize(5);
+        dataSource.setMinIdle(5);
+        dataSource.setMaxActive(20);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestOnReturn(false);
+        dataSource.setKeepAlive(true);
+        dataSource.setMinEvictableIdleTimeMillis(300000L);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+        dataSource.setUseGlobalDataSourceStat(true);
+
+        Properties connectProperties = new Properties();
+        connectProperties.setProperty("druid.stat.mergeSql", "true");
+        connectProperties.setProperty("druid.stat.slowSqlMillis", "5000");
+        connectProperties.setProperty("useCursorFetch", "true");
+        connectProperties.setProperty("defaultFetchSize", "1000");
+        dataSource.setConnectProperties(connectProperties);
+
+        StatFilter statFilter = new StatFilter();
+        statFilter.setDbType(DbType.mysql);
+        statFilter.setLogSlowSql(true);
+        statFilter.setSlowSqlMillis(2000);
+        statFilter.setMergeSql(true);
+
+        WallConfig wallConfig = new WallConfig();
+        wallConfig.setMultiStatementAllow(true);
+        WallFilter wallFilter = new WallFilter();
+        wallFilter.setDbType(DbType.mysql);
+        wallFilter.setConfig(wallConfig);
+
+        List<com.alibaba.druid.filter.Filter> filters = new ArrayList<>(2);
+        filters.add(statFilter);
+        filters.add(wallFilter);
+        dataSource.setProxyFilters(filters);
+        return dataSource;
     }
 
-    /**
-     * 创建 MyBatis-Plus 的 SqlSessionFactory
-     */
     @Primary
     @Bean(name = "mysqlSqlSessionFactory")
     public SqlSessionFactory mysqlSqlSessionFactory(
@@ -72,28 +92,20 @@ public class MybatisConfig {
         MybatisSqlSessionFactoryBean sessionFactory = new MybatisSqlSessionFactoryBean();
         sessionFactory.setDataSource(dataSource);
         sessionFactory.setTypeAliasesPackage("com.freesia.*.po");
-        // Mapper XML 文件位置（如果有）
         sessionFactory.setMapperLocations(
                 new PathMatchingResourcePatternResolver()
                         .getResources("classpath*:/mapper/*.xml")
         );
-        // 添加 MyBatis-Plus 插件（如分页插件）
         sessionFactory.setPlugins(mybatisPlusInterceptor);
         return sessionFactory.getObject();
     }
 
-    /**
-     * 创建 MyBatis 事务管理器（供 MyBatis-Plus 使用）
-     */
     @Bean(name = "mybatisTransactionManager")
     public DataSourceTransactionManager mybatisTransactionManager(
             @Qualifier("mysqlDataSource") DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
 
-    /**
-     * Mybatis-Plus拦截器
-     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
@@ -103,24 +115,14 @@ public class MybatisConfig {
         return interceptor;
     }
 
-    /**
-     * 全局修改拦截器
-     */
     public BlockAttackInnerInterceptor blockAttackInnerInterceptor() {
         return new BlockAttackInnerInterceptor();
     }
 
-    /**
-     * 分页插件拦截器
-     * DbType：数据库类型(根据类型获取应使用的分页方言)
-     */
     public PaginationInnerInterceptor paginationInnerInterceptor() {
-        return new PaginationInnerInterceptor(DbType.MYSQL);
+        return new PaginationInnerInterceptor(com.baomidou.mybatisplus.annotation.DbType.MYSQL);
     }
 
-    /**
-     * 乐观锁插件拦截器
-     */
     public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor() {
         return new OptimisticLockerInnerInterceptor();
     }
--- freesia-common/freesia-common-jdbc/src/main/java/com/freesia/jdbc/config/PgJpaConfig.java
+++ freesia-common/freesia-common-jdbc/src/main/java/com/freesia/jdbc/config/PgJpaConfig.java
@@ -1,63 +1,82 @@
 package com.freesia.jdbc.config;
 
+import com.alibaba.druid.DbType;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Properties;
 
-/**
- * @author Evad.Wu
- * @Description PostgreSQL 配置类
- * @date 2026-08-02
- */
 @AutoConfiguration
 @AutoConfigureBefore(JpaBaseConfiguration.class)
 @EnableJpaRepositories(basePackages = {
 }, entityManagerFactoryRef = "pgsqlEntityManagerFactory", transactionManagerRef = "pgsqlTransactionManager")
 public class PgJpaConfig {
-    /**
-     * 创建 PostgreSQL 数据源
-     */
     @Bean(name = "pgsqlDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.pgsql")
-    public DataSource pgsqlDataSource() {
-        return DataSourceBuilder.create().build();
+    public DruidDataSource pgsqlDataSource(Environment environment) {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setName("pgsql");
+        dataSource.setDriverClassName(environment.getRequiredProperty("spring.datasource.pgsql.driver-class-name"));
+        dataSource.setUrl(environment.getRequiredProperty("spring.datasource.pgsql.jdbc-url"));
+        dataSource.setUsername(environment.getRequiredProperty("spring.datasource.pgsql.username"));
+        dataSource.setPassword(environment.getRequiredProperty("spring.datasource.pgsql.password"));
+        dataSource.setDbType(DbType.postgresql);
+        dataSource.setInitialSize(5);
+        dataSource.setMinIdle(5);
+        dataSource.setMaxActive(20);
+        dataSource.setValidationQuery("SELECT 1");
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestOnReturn(false);
+        dataSource.setKeepAlive(true);
+        dataSource.setMinEvictableIdleTimeMillis(300000L);
+        dataSource.setPoolPreparedStatements(true);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(20);
+        dataSource.setUseGlobalDataSourceStat(true);
+
+        Properties connectProperties = new Properties();
+        connectProperties.setProperty("druid.stat.mergeSql", "true");
+        connectProperties.setProperty("druid.stat.slowSqlMillis", "5000");
+        dataSource.setConnectProperties(connectProperties);
+
+        StatFilter statFilter = new StatFilter();
+        statFilter.setDbType(DbType.postgresql);
+        statFilter.setLogSlowSql(true);
+        statFilter.setSlowSqlMillis(2000);
+        statFilter.setMergeSql(true);
+        dataSource.setProxyFilters(List.of(statFilter));
+
+        return dataSource;
     }
 
-    /**
-     * 创建 JPA EntityManagerFactory
-     */
     @Bean(name = "pgsqlEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean pgsqlEntityManagerFactory(
             EntityManagerFactoryBuilder builder,
-            @Qualifier("pgsqlDataSource") DataSource dataSource) {
+            @Qualifier("pgsqlDataSource") DruidDataSource dataSource) {
         Map<String, Object> properties = new HashMap<>(16);
-        // Hibernate 方言（PostgreSQL）
         properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        // 禁止 Hibernate 启动时自动执行 DDL（如 alter table），避免擅自修改表结构
         properties.put("hibernate.hbm2ddl.auto", "none");
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "true");
-        // 物理命名策略（可选项，将驼峰转为下划线）
         properties.put("hibernate.physical_naming_strategy",
                 "org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy");
 
         return builder
                 .dataSource(dataSource)
-                // PostgreSQL JPA 实体类扫描路径（与 MySQL 实体隔离）
                 .packages("com.freesia.po.pgsql",
                         "com.freesia.account.po.pgsql",
                         "com.freesia.icon.po.pgsql",
@@ -68,9 +87,6 @@ public class PgJpaConfig {
                 .build();
     }
 
-    /**
-     * 创建 PostgreSQL 事务管理器
-     */
     @Bean(name = "pgsqlTransactionManager")
     public PlatformTransactionManager pgsqlTransactionManager(
             @Qualifier("pgsqlEntityManagerFactory") LocalContainerEntityManagerFactoryBean entityManagerFactory) {
